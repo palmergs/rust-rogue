370a1e36593f7d168ed1b902b7a86c9c4c24351c
Galen Palmer <galen.palmer@contrastsecurity.com>
2017-06-12 22:43:17 -0400

    can generate map

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 extern crate tcod;
+extern crate rand;
+
+use std::cmp;
+use rand::Rng;
 
 use tcod::console::*;
 use tcod::Color;
@@ -6,9 +10,61 @@ use tcod::colors;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
 
+type Map = Vec<Vec<Tile>>;
+
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
+
+const MAP_WIDTH: i32 = SCREEN_WIDTH;
+const MAP_HEIGHT: i32 = SCREEN_HEIGHT - 5;
+
 const LIMIT_FPS: i32 = 20;
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, block_sight: false }
+    }
+
+    pub fn wall() -> Self {
+        Tile { blocked: true, block_sight: true }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2) && (self.x2 >= other.x1) && 
+                (self.y1 <= other.y2) && (self.y2 >= other.y1)
+    }
+}
 
 struct Object {
     x: i32,
@@ -42,6 +98,25 @@ impl Object {
     }
 }
 
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    for object in objects {
+        object.draw(con);
+    }
+
+    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
+
 fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
     let key = root.wait_for_keypress(true);
     match key {
@@ -59,6 +134,68 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
     false
 }
 
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+
+}
+
+fn make_map() -> (Map, (i32, i32)) {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // let room1 = Rect::new(20, 15, 10, 15);
+    // let room2 = Rect::new(50, 15, 10, 15);
+    // create_room(room1, &mut map);
+    // create_room(room2, &mut map);
+    // create_h_tunnel(25, 55, 23, &mut map);
+
+    let mut starting_position = (0, 0);
+
+    let mut rooms = vec![];
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+        let new_room = Rect::new(x, y, w, h);
+        let failed = rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+        if !failed {
+            create_room(new_room, &mut map);
+            let (new_x, new_y) = new_room.center();
+            if rooms.is_empty() {
+                starting_position = (new_x, new_y);
+            } else {
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+            rooms.push(new_room);
+        }
+    }
+
+    (map, starting_position)
+}
+
 fn main() {
     let mut root = Root::initializer()
             .font("arial10x10.png", FontLayout::Tcod)
@@ -70,13 +207,8 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
-
-    let player = Object::new(SCREEN_WIDTH / 2, 
-            SCREEN_HEIGHT / 2, 
-            '@', 
-            colors::WHITE);
+    let (map, (player_x, player_y)) = make_map();
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, 
             SCREEN_HEIGHT / 2, 
             '@', 
@@ -84,7 +216,8 @@ fn main() {
     let mut objects = [ player, npc ];
 
     while !root.window_closed() {
-        con.set_default_foreground(colors::WHITE);
+        render_all(&mut root, &mut con, &objects, &map);
+        root.flush();
 
         for object in &objects {
             object.clear(&mut con)
@@ -94,17 +227,5 @@ fn main() {
         if exit {
             break
         }
-
-        for object in &objects {
-            object.draw(&mut con)
-        }
-
-        blit(&mut con, 
-                (0, 0), 
-                (SCREEN_WIDTH, SCREEN_HEIGHT), 
-                &mut root, 
-                (0, 0), 
-                1.0, 1.0);
-        root.flush();
     }
 }
