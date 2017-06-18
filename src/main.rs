25ed91cd65b94b26ec34207522ad81639597871e
Galen Palmer <palmergs@gmail.com>
2017-06-18 18:09:22 -0400

    main menu

--- src/main.rs
+++ src/main.rs
@@ -431,7 +431,11 @@ fn inventory_menu(inventory: &[Object], header: &str, root: &mut Root) -> Option
 
 fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32, root: &mut Root) -> Option<usize> {
     assert!(options.len() <= 26, "Cannot have a menu with more than 26 options.");
-    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let header_height = if header.is_empty() {
+        0
+    } else {
+        root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header)
+    };
     let height = options.len() as i32 + header_height;
 
     let mut window = Offscreen::new(width, height);
@@ -878,6 +882,8 @@ fn initialize_fov(map: &Map, tcod: &mut Tcod) {
                     !map[x as usize][y as usize].blocked);
         }
     }
+
+    tcod.con.clear();
 }
 
 fn play_game(objects: &mut Vec<Object>, game: &mut Game, tcod: &mut Tcod) {
@@ -916,6 +922,25 @@ fn play_game(objects: &mut Vec<Object>, game: &mut Game, tcod: &mut Tcod) {
     }
 }
 
+fn main_menu(tcod: &mut Tcod) {
+    let img = tcod::image::Image::from_file("menu_background.png").ok().expect("Background image not found");
+    while !tcod.root.window_closed() {
+        tcod::image::blit_2x(&img, (0, 0), (-1, -1), &mut tcod.root, (0, 0));
+        let choices = &["Play a new game", "Continue last game", "Quit"];
+        let choice = menu("", choices, 24, &mut tcod.root);
+        match choice {
+            Some(0) => {
+                let (mut objects, mut game) = new_game(tcod);
+                play_game(&mut objects, &mut game, tcod);
+            },
+            Some(2) => {
+                break;
+            },
+            _ => {}
+        }
+    }
+}
+
 fn main() {
 
     let root = Root::initializer()
@@ -935,7 +960,11 @@ fn main() {
         mouse: Default::default(),
     };
 
-    let (mut objects, mut game) = new_game(&mut tcod);
-    play_game(&mut objects, &mut game, &mut tcod);
+    // let (mut objects, mut game) = new_game(&mut tcod);
+    // play_game(&mut objects, &mut game, &mut tcod);
+    tcod.root.set_default_foreground(colors::LIGHT_YELLOW);
+    tcod.root.print_ex(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 4, BackgroundFlag::None, TextAlignment::Center, "TOMB OF THE ANCIENT KINGS");
+    tcod.root.print_ex(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 - 2, BackgroundFlag::None, TextAlignment::Center, "By Me");
+    main_menu(&mut tcod);
 
 }
